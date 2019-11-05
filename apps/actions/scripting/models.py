347e12297b5dd362ab9985f5398144d306b43d6f
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-11-04 23:13:10 -0300

    Modificando modelos de roteirização "scripting";

--- apps/actions/scripting/models.py
+++ apps/actions/scripting/models.py
@@ -14,22 +14,35 @@ STATUS = (
     (COMPLETED, 'Concluído'),
 )
 
+
 class Scripting(models.Model):
     description = models.CharField(_('Descrição'), max_length=300)
-    date_initial = models.DateField(_('Início'), null=True)
-    date_final = models.DateField(_('Fim'), null=True)
-    date_registred = models.DateField(_('Data de Cadastro'), default=datetime.date.today)
-    origin_latitude = models.CharField(_('Origen Latitude'), max_length=100, null=True)
-    origin_longitude = models.CharField(_('Origen Longitude'), max_length=100, null=True)
-    origin_address = models.CharField(_('Origen Endereço'), max_length=300, null=True)
-    destiny_latitude = models.CharField(_('Destino Latitude'), max_length=100, null=True)
-    destiny_longitude = models.CharField(_('Destino Longitude'), max_length=100, null=True)
-    destiny_address = models.CharField(_('Destino Endereço'), max_length=300, null=True)
-    status = models.CharField(_('Situação'), max_length=1, choices=STATUS, default=PEDDING)
+    date_initial = models.DateField(_('Data Início'), null=True, blank=True)
+    hour_initial = models.TimeField(_('Hora Início'), null=True, blank=True)
+    date_final = models.DateField(_('Data Fim'), null=True, blank=True)
+    hour_final = models.TimeField(_('Hora Fim'), null=True, blank=True)
+    date_registred = models.DateField(
+        _('Data de Cadastro'), default=datetime.date.today)
+    origin_latitude = models.CharField(
+        _('Origen Latitude'), max_length=100, null=True)
+    origin_longitude = models.CharField(
+        _('Origen Longitude'), max_length=100, null=True)
+    origin_address = models.CharField(
+        _('Origen Endereço'), max_length=300, null=True)
+    destiny_latitude = models.CharField(
+        _('Destino Latitude'), max_length=100, null=True)
+    destiny_longitude = models.CharField(
+        _('Destino Longitude'), max_length=100, null=True)
+    destiny_address = models.CharField(
+        _('Destino Endereço'), max_length=300, null=True)
+    status = models.CharField(
+        _('Situação'), max_length=1, choices=STATUS, default=PEDDING)
 
-    localizations = models.ManyToManyField('localization.Localization', 
-        verbose_name='localization', related_name="scripting", blank=True)
-    company = models.ForeignKey('company.Company', verbose_name=_('Empresa'), 
+    localizations = models.ManyToManyField('localization.Localization',
+                                           verbose_name='localization', related_name="scripting", blank=True)
+    employees = models.ManyToManyField('employee.Employee',
+                                       verbose_name='employee', related_name="scripting", blank=True)
+    company = models.ForeignKey('company.Company', verbose_name=_('Empresa'),
                                 related_name='scriptings', on_delete=models.CASCADE)
 
     @property
--- apps/actions/scripting/serializers.py
+++ apps/actions/scripting/serializers.py
@@ -6,6 +6,14 @@ from .models import Scripting
 
 class ScriptingSerializer(BaseSerializer):
     localizations_count = serializers.CharField(read_only=True)
+    date_initial = serializers.DateField(
+        required=False, allow_null=True, format='%Y-%m-%d', input_formats=['', '%d/%m/%Y', '%Y-%m-%d'])
+    hour_initial = serializers.TimeField(
+        required=False, allow_null=True, format='%H:%M', input_formats=['', '%H:%M'])
+    date_final = serializers.DateField(
+        required=False, allow_null=True, format='%Y-%m-%d', input_formats=['', '%d/%m/%Y', '%Y-%m-%d'])
+    hour_final = serializers.TimeField(
+        required=False, allow_null=True, format='%H:%M', input_formats=['', '%H:%M'])
 
     class Meta:
         model = Scripting

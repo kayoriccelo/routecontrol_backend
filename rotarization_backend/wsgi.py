de45f08754f3622bd8c3ce5aab58d0be8921fa5a
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-11-05 00:19:06 -0300

    Ajuste nomenclatura.

--- rotarization_backend/wsgi.py
+++ rotarization_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rotarizarion_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rotarization_backend.settings')
 
 application = get_wsgi_application()
